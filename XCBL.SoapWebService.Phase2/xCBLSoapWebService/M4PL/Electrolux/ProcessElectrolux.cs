5d2902e3e1bbd2d6b9e8906117d5ae61ff0277e3
manoj.kumar <manoj.kumar@DOLAP122>
2020-04-27 12:44:37 +0530

    Log Electrolux

--- XCBL.SoapWebService.Phase2/xCBLSoapWebService/M4PL/Electrolux/ProcessElectrolux.cs
+++ XCBL.SoapWebService.Phase2/xCBLSoapWebService/M4PL/Electrolux/ProcessElectrolux.cs
@@ -26,12 +26,14 @@ namespace xCBLSoapWebService.M4PL.Electrolux
             XCBL_User xCblServiceUser = new XCBL_User();
             if (CommonProcess.IsAuthenticatedRequest(currentOperationContext, ref xCblServiceUser))
             {
-                MeridianSystemLibrary.LogTransaction(xCblServiceUser.WebUsername, xCblServiceUser.FtpUsername, "IsAuthenticatedRequest", "01.02", "Success - Authenticated request", "Electrolux Process", "No FileName", "No Electrolux ID", "No Order Number", null, "Success");
+                MeridianSystemLibrary.LogTransaction(xCblServiceUser.WebUsername, xCblServiceUser.FtpUsername, "Electrolux:IsAuthenticatedRequest", "03.01", "Success - Authenticated request", "Electrolux Process", "No FileName", "No Electrolux ID", "No Order Number", null, "Success");
 
                 var requestContext = currentOperationContext.RequestContext;
                 var requestMessage = requestContext.RequestMessage.ToString();//.ReplaceSpecialCharsWithSpace();
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.LoadXml(requestMessage);
+
+                MeridianSystemLibrary.LogTransaction(xCblServiceUser.WebUsername, xCblServiceUser.FtpUsername, "Electrolux:ElectroluxRequest", "03.02", "Electrolux request logging", "Electrolux Process", "No FileName", "No Electrolux ID", "No Order Number", xmlDoc, "Success");
 
                 XmlNodeList requisitionNode_xml = xmlDoc.GetElementsByTagName("fxEnvelope");//Http Request creating this tag
 
@@ -52,8 +54,14 @@ namespace xCBLSoapWebService.M4PL.Electrolux
                         var response = M4PL.M4PLService.CallM4PLAPI<List<OrderResponseResult>>(electroluxOrderDetails, "XCBL/Electrolux/OrderRequest",isElectrolux: true);
                         if (response != null)
                         {
-                            _meridianResult.Status = MeridianGlobalConstants.MESSAGE_ACKNOWLEDGEMENT_SUCCESS;
+                            string responseString = GetXMLFromObject(response);
+                            XmlDocument responsexmlDoc = new XmlDocument();
+                            responsexmlDoc.LoadXml(responseString);
 
+                            MeridianSystemLibrary.LogTransaction(xCblServiceUser.WebUsername, xCblServiceUser.FtpUsername, "Electrolux:ElectroluxResponse", "03.03", "Electrolux response logging", "Electrolux Process", "No FileName", "No Electrolux ID", "No Order Number", responsexmlDoc, "Success");
+                            _meridianResult.Status = MeridianGlobalConstants.MESSAGE_ACKNOWLEDGEMENT_SUCCESS;
+                            if(response.Any() && response.Count> 0)
+                            _meridianResult.ResultObject = response[0];
                         }
                     }
                 }
@@ -63,7 +71,7 @@ namespace xCBLSoapWebService.M4PL.Electrolux
             else
             {
                 _meridianResult.Status = MeridianGlobalConstants.MESSAGE_ACKNOWLEDGEMENT_FAILURE;
-                MeridianSystemLibrary.LogTransaction("No WebUser", "No FTPUser", "IsAuthenticatedRequest", "03.01", "Error - New SOAP Request not authenticated", "UnAuthenticated Request", "No FileName", "No Requisition ID", "No Order Number", null, "Error 03.01 - Invalid Credentials");
+                MeridianSystemLibrary.LogTransaction("No WebUser", "No FTPUser", "IsAuthenticatedRequest", "03.04", "Error - New SOAP Request not authenticated", "UnAuthenticated Request", "No FileName", "No Requisition ID", "No Order Number", null, "Error 03.01 - Invalid Credentials");
             }
             return _meridianResult;
         }
@@ -91,6 +99,31 @@ namespace xCBLSoapWebService.M4PL.Electrolux
                 return xElement;
             }
             return new XElement(xmlDocument.Name.LocalName, xmlDocument.Elements().Select(el => RemoveAllNamespaces(el)));
+        }
+
+        public static string GetXMLFromObject(object o)
+        {
+            StringWriter sw = new StringWriter();
+            XmlTextWriter tw = null;
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(o.GetType());
+                tw = new XmlTextWriter(sw);
+                serializer.Serialize(tw, o);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sw.Close();
+                if (tw != null)
+                {
+                    tw.Close();
+                }
+            }
+            return sw.ToString();
         }
 
     }
